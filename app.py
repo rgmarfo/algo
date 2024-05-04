a82927b1855743dc64608cf40b0fe53b30dfe922
Gyekye <mrgyekye740@gmail.com>
2024-05-04 11:46:25 +0000

    add bisection method

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sympy import symbols, lambdify
+from bisection import bisection_method
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("pages/index.html")
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    equation_str = request.form['equation']
+    a = float(request.form['a'])
+    b = float(request.form['b'])
+    try:
+        root, data = bisection_method(equation_str,a, b,)
+
+        # Generate the step
+        steps_html = f"<h3> Showing the first 5 steps </h3><h3>The function f(x) = {equation_str}</h3>\n<br/><br/>"
+        for iteration, root_estimate, func_value in data[:5]:
+            steps_html += f"Iteration {iteration+1}: <br>"
+            steps_html += f"Root Estimate = ({a} + {b}) / 2 = {root_estimate}<br>"
+            steps_html += f"Function Value at Root Estimate = f({root_estimate}) = {func_value}<br>"
+            steps_html += f"---<br>"
+        
+        # Generate table for the first 10 iterations
+        table_html = '<table><tr><th>Iteration</th><th>Root Estimate</th><th>Function Value at Root Estimate</th></tr>'
+        for iteration, root_estimate, func_value in data[:10]:
+            table_html += f'<tr><td>{iteration+1}</td><td>{root_estimate}</td><td>{func_value}</td></tr>'
+        table_html += '</table>'
+        
+        return f"<h3>The root of the equation: {root}</h3><h4>Steps:</h4>{steps_html}<h4>First 10 iterations:</h4>{table_html}"
+    except ValueError as e:
+        return str(e)
 
 @app.route("/feedback", methods=['GET'])
 def bisection():
---
+++ bisection.py
@@ -0,0 +1,27 @@
+from sympy import symbols, lambdify
+
+def bisection_method(equation_str, a, b, tol=1e-6, max_iter=1000):
+    iterations = 0
+    data = []
+
+    x = symbols('x')
+    equation = lambdify(x, equation_str)
+
+    if equation(a) * equation(b) >= 0:
+        raise ValueError("Bisection method fails.")
+
+    while (b - a) / 2 > tol and iterations < max_iter:
+        c = (a + b) / 2
+        root_estimate = (a + b) / 2
+        func_value = equation(root_estimate)
+        data.append((iterations, root_estimate, func_value))
+        
+        if func_value == 0:
+            break
+        if equation(c) * equation(a) < 0:
+            b = c
+        else:
+            a = c
+        iterations += 1
+
+    return root_estimate, data
